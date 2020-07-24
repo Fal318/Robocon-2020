f12b2fe3856f89daa0deff1d3aedc3fe2b48f992
Fal318 <uznaytiuy0625@gmail.com>
2020-07-24 11:50:56 +0900

    ディレクトリの分割と整理

---
+++ Server/RN42.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+# RN42.py
+from time import sleep
+import sys
+import bluetooth
+
+
+class RN42:
+    def __init__(self, name, addr, num):
+        """ Arduino with RN42"""
+        self.__name = name  # Set Name
+        self.__bdAddr = addr  # the address form the Arduino RN42
+        self.__port = num  # Connect Port (RaspberryPi  to Arduino)
+        self.__sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)  # config
+
+    @property
+    def name(self):
+        pass
+
+    @name.getter
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        self.__name = name
+
+    @property
+    def bdAddr(self):
+        pass
+
+    @bdAddr.getter
+    def bdAddr(self):
+        return self.__bdAddr
+
+    @bdAddr.setter
+    def bdAddr(self, value):
+        self.__bdAddr = value
+        return
+
+    @property
+    def port(self):
+        pass
+
+    @port.getter
+    def port(self):
+        return self.__port
+
+    @port.setter
+    def port(self, value):
+        self.__port = value
+        return
+
+    @property
+    def sock(self):
+        pass
+
+    @sock.getter
+    def sock(self):
+        return self.__sock
+
+    @sock.setter
+    def sock(self, value):
+        self.__sock = value
+
+    def reConnect(self, addr, num):
+        self.__bdAddr = addr  # the address form the Arduino RN42
+        self.__port = num  # Connect Port (RaspberryPi  to Arduino)
+        self.__sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)  # config
+
+    def connectBluetooth(self, bdAddr, port):
+        #for _ in range(4):
+        while True:
+            try:
+                self.__sock.connect((bdAddr, port))
+                sleep(2)
+                break
+            except bluetooth.BluetoothError:
+                self.reConnect(bdAddr, port)
+                sleep(0.5)
+            except KeyboardInterrupt:
+                break
+
+    def disConnect(self, sock=None):
+        self.__sock.close()
+        # sock.close()
---
+++ Server/address.py
@@ -0,0 +1,6 @@
+client_address = [
+    "B8:27:EB:2F:10:B6",
+    "DC:A6:32:70:DB:03",
+    
+]
+server_address = "A0:AF:BD:9A:F5:D2"
