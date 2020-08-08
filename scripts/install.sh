9f323767e5e0a75189b182f7378c21a20b794e60
Fal318 <uznaytiuy0625@gmail.com>
2020-08-08 14:33:18 +0900

    :horse:環境構築用スクリプトの冗長な部分を削除

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,15 +1,9 @@
 #! /bin/bash
 yes | sudo apt update 
 yes | sudo apt upgrade -y
-yes | sudo install bluetooth
-yes | sudo apt install libusb-dev
-yes | sudo apt install libdbus-1-dev
-yes | sudo apt install libglib2.0-dev
-yes | sudo apt install libudev-dev
-yes | sudo apt install libical-dev
-yes | sudo apt install libreadline-dev
-yes | sudo apt install libdbus-glib-1-dev
-yes | sudo apt install libbluetooth-dev
+yes | sudo apt install bluetooth libusb-dev libdbus-1-dev \
+libglib2.0-dev libudev-dev libical-dev libreadline-dev \
+libdbus-glib-1-dev libbluetooth-dev
 wget http://www.kernel.org/pub/linux/bluetooth/bluez-5.54.tar.xz
 tar Jxfv bluez-5.54.tar.xz
 cd ./bluez-5.54/ || exit
