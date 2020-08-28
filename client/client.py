c960618c8b364f19c3435cfe8050a3192ab7bd58
Fal318 <uznaytiuy0625@gmail.com>
2020-08-29 01:14:19 +0900

    :construction:WIP

--- client/client.py
+++ client/client.py
@@ -1,25 +1,36 @@
 # -*- coding: utf-8 -*-
 """クライアント側の代用プログラム"""
+import time
+import csv
 import bluetooth as bt
 
 
 def main():
     """main"""
-    server_socket = bt.BluetoothSocket(bt.RFCOMM)
 
+    server_socket = bt.BluetoothSocket(bt.RFCOMM)
+    recv_data = []
     try:
         server_socket.bind(("", 1))
         server_socket.listen(1)
         client_socket = server_socket.accept()[0]
         print("Connect")
-        rcv = None
-        while rcv != 0:
-            rcv = int.from_bytes(client_socket.recv(1024), "little")
+        while True:
+            rcv = int.from_bytes(client_socket.recv(64), "little")
+            recv_data.append([time.time(), rcv])
             print(f"recv:{rcv}")
+            if rcv == 0:
+                break
     except KeyboardInterrupt:
         print("Connection Killed")
     else:
         print("Connection Ended")
+
+    file = open("../data/log/cdata.csv", "w")
+
+    w = csv.writer(file)
+    w.writerows(recv_data)
+    file.close()
 
 
 if __name__ == "__main__":
