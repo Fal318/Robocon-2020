d59362330dd3054f0ea031f7bb70fc37963f4b0d
Fal318 <uznaytiuy0625@gmail.com>
2020-09-23 16:45:16 +0900

    :construction: WIP

---
+++ master/library/timestamp.py
@@ -0,0 +1,18 @@
+import time
+
+
+class Timestamp:
+    def __init__(self, length: int):
+        self.__length = length
+        self.__aivable = [False for _ in range(length)]
+        self.ts: int = 0
+
+    def change_aivable(self, index: int):
+        if index >= self.__length:
+            raise ValueError("index >= TARGET")
+        self.__aivable[index] = True
+
+    def timestamp(self) -> int:
+        if not False in self.__aivable:
+            self.ts = int((time.time()+2)*10000000)
+        return self.ts
--- master/main.py
+++ master/main.py
@@ -6,7 +6,7 @@ import threading
 import bluetooth as bt
 import address as ad
 from library import bt_connect
-from library import fix_lag
+from library import timestamp as ts
 
 
 args = sys.argv
@@ -15,26 +15,16 @@ if len(args) > 1 and args[1] == "-d":
     IS_DEBUG = True
 
 
-TARGET: int = 2
-PERIOD: float = 0.1
-LONG: int = 1000
-"""
-TARGET:接続する台数
-PERIOD:実行周期(sec)
-"""
+TARGET: int = 2  # TARGET:接続する台数
 
-fix_lag = fix_lag.FixLag()
+timestamp = ts.Timestamp()
 
 
 class Connection:
     """通信を定周期で行う"""
 
     def __init__(self, proc_id: int):
-        self.__send_data: list = list(range(LONG))  # 送るデータ
-        self.__sendtime: float = None  # 最後に送信をした時間
         self.__proc_id: int = proc_id  # プロセスを識別するID
-        self.__lag: int = 0
-        self.__ended: bool = False
 
         try:
             # 通信用のインスタンスを生成
@@ -56,22 +46,22 @@ class Connection:
 
     def __send(self, data: int):
         """データ(整数値)を送信する"""
-        self.__sendtime = time.time()
         self.__ras.sock.send((data).to_bytes(1, "little"))
         print("target={0} send:{1}".format(self.__proc_id, data))
 
     def read(self):
-        """データの受信"""
-        while not self.__ended:
-            fix_lag.add(self.__proc_id, int.from_bytes(
-                self.__ras.sock.recv(64), "little")/10000000)
+        return int.from_bytes(
+            self.__ras.sock.recv(1), "little")
 
-    def main_process(self, period: int):
+    def main_process(self):
         """メインプロセス"""
         try:
-            for send in self.__send_data[0]:
-                self.__send(send)
-                time.sleep(period - (time.time() - self.__sendtime)+self.__lag)
+            if not self.read():
+                raise Exception("")
+            timestamp.change_aivable(self.__proc_id)
+            while not timestamp.timestamp():
+                continue
+            self.__send(timestamp.timestamp())
         except KeyboardInterrupt:
             self.__send(-1)
             print("Connection Killed")
@@ -79,18 +69,9 @@ class Connection:
             self.__send(-1)
             print("周期が早すぎます")
         except bt.BluetoothError:
-            print("Connection Killed")
+            print("Connection Kt illed")
         else:
             self.__send(-1)
-        finally:
-            self.__ended = True
-
-    def change_lag(self):
-        """ラグの変更"""
-        while not self.__ended:
-            time.sleep(1)
-            self.__lag = fix_lag.get_lag(self.__proc_id) / 10
-            # 10ループ(1sec)で修正するので10で割る
 
     def __del__(self):
         self.__ras.disconnect()
@@ -105,10 +86,7 @@ def main():
         ras = Connection(i)
         if ras.is_aivable():
             ras_arr.append(ras)
-            threads.append(threading.Thread(
-                target=ras.main_process, args=([PERIOD])))
-            threads.append(threading.Thread(target=ras.read))
-            threads.append(threading.Thread(target=ras.change_lag))
+            threads.append(threading.Thread(target=ras.main_process))
 
     for thread in threads:
         thread.start()
