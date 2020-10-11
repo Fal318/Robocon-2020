be82935f6e15e5e52bb80cd64c76a246674167a0
Fal318 <uznaytiuy0625@gmail.com>
2020-10-11 10:13:54 +0900

    :wrench:実行用スクリプトの統合

--- scripts/run/master.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sudo ntpdate -b 192.168.10.10
-cd ../../master/
-python3 main.py
--- scripts/run/run.sh
+++ scripts/run/run.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 sudo ntpdate -b 192.168.10.10
 name=$HOSTNAME
-cd ../../client
 if [ $name == "ukulele" ]; then
+    cd ../../client
     python3 client_u.py
 elif [ $name == "percussion" ]; then
+    cd ../../client
     python3 client_p.py
+elif [ $name == "laptop" ]; then
+    cd ../../master
+    python3 main.py
 fi
