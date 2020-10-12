2563ae42f52ff4e5b3ccd1c0317c03ebd62a208f
Fal318 <uznaytiuy0625@gmail.com>
2020-10-12 16:33:27 +0900

    :wrench:実行用スクリプトのアップデート

--- scripts/run/run.sh
+++ scripts/run/run.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-sudo ntpdate -b 192.168.10.10
 name=$HOSTNAME
-if [ $name == "ukulele" ]; then
-    cd ../../client
-    python3 client_u.py
-elif [ $name == "percussion" ]; then
-    cd ../../client
-    python3 client_p.py
-elif [ $name == "laptop" ]; then
+if [ $name == "laptop" ]; then
     cd ../../master
     python3 main.py
+else
+    sudo ntpdate -b 192.168.10.5
+    if [ $name == "ukulele" ]; then
+        cd ../../client
+        python3 client_u.py
+    elif [ $name == "percussion" ]; then
+        cd ../../client
+        python3 client_p.py
+    fi
 fi
