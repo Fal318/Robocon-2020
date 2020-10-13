02c1a24ff6e697534253282df0a4aae464b89bbd
Fal318 <uznaytiuy0625@gmail.com>
2020-10-13 09:02:20 +0900

    :books:READMEのアップデート

--- scripts/setup/install.sh
+++ scripts/setup/install.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 sudo apt update 
 sudo apt upgrade -y
-sudo apt install -y bluez bluetooth libusb-dev libdbus-1-dev \
-libglib2.0-dev libudev-dev libical-dev libreadline-dev \
-libdbus-glib-1-dev libbluetooth-dev ntpdate
+sudo apt install -y bluez bluetooth libbluetooth-dev 
+libusb-dev libdbus-1-dev libglib2.0-dev libudev-dev \
+libical-dev libreadline-dev libdbus-glib-1-dev  ntp ntpdate
 pip3 install setuptools --user
 pip3 install pandas pybluez pyserial --user
